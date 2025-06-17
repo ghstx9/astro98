1468ca66b1ba89425462d16a35d4e474e0e42d55
ghstx9 <sorrowprivategt@gmail.com>
2025-06-17 09:43:32 +0700

    changed calculator icon to a real win98 asset

--- src/components/window/my-computer.jsx
+++ src/components/window/my-computer.jsx
@@ -48,8 +48,8 @@ const MyComputerApp = () => {
     'C:/Windows': [
       { name: 'System', type: 'folder', icon: <img src="/icons/typefolder.png" alt="Sys" className="w-4 h-4 inline" />, size: '', modified: '25/10/1998' },
       { name: 'System32', type: 'folder', icon: <img src="/icons/typefolder.png" alt="Sys32" className="w-4 h-4 inline" />, size: '', modified: '25/10/1998' },
-      { name: 'notepad.exe', type: 'file', icon: <img src="/icons/notepad16.png" alt="notepad" className="w-4 h-4 inline" />, size: '48 KB', modified: '25/10/1998' },
-      { name: 'calc.exe', type: 'file', icon: '🧮', size: '32 KB', modified: '25/10/1998' }
+      { name: 'notepad.exe', type: 'file', icon: <img src="/icons/notepad16.png" alt="notepad" className="w-4 h-4 inline" />, size: '14 KB', modified: '25/10/1998' },
+      { name: 'calc.exe', type: 'file', icon: <img src="icons/calc16.png" alt="calc" className="w-4 h-4 inline" />, size: '102 KB', modified: '25/10/1998' }
     ],
     'C:/Windows/System': [
       { name: 'Speech', type: 'folder', icon: '📁', size: '', modified: '25/10/1998' }
